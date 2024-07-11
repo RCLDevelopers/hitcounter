5d2ca5c021d70d354f05061cec5f75a2f0d4b407
RCLDevelopers <rasshtech@gmail.com>
2024-07-11 08:02:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>PHP ZANGTICS| DEMO</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="navbar-header">
+        <a class="navbar-brand" rel="home" href="http://www.zangticsdigital.com/">zangtics digital  | Programing Blog</a>
+		
+
+</nav>
+
+<div class="container-fluid">
+  
+  <!--left-->
+
+     
+ <!--/left-->
+  
+  <!--center-->
+  <div class="col-sm-9">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3>Website Hit Counter in PHP</h3>
+ <hr>
+<h4>When you refresh the page hitcount print the value with 1 Increment</h4>
+		
+Hit counts : <?php include("counter.php");?>
+      </div>
+    </div>
+    <hr>
+        
+   
+  </div><!--/center-->
+
+  <!--right-->
+  <div class="col-sm-3">
+
+    	<div class="panel panel-default">
+       
+         	<div class="panel-body"></div>
+        </div>
+   
+  </div>
+<!--/right-->
+  <hr>
+</div><!--/container-fluid-->
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
